77613379e55d0de80377711e646f952e72c7b85c
mgt581 <alexbryantwork3234@outlook.com>
2025-12-05 16:03:09 +0000

    Create aiKeys.js

---
+++ aiKeys.js
@@ -0,0 +1,10 @@
+// aiKeys.js - Keep this file out of version control
+// This is where we store API keys safely.
+
+const AI_KEYS = {
+  removeBackgroundApiKey: "your-remove-background-api-key-here",
+  rebackgroundApiKey: "your-rebackground-api-key-here",
+  magicEraseApiKey: "your-magic-erase-api-key-here"
+};
+
+export default AI_KEYS;
